864efad4544e38c02f83fb2955900646a3b187ef
Cecilius Sousa <ceciliusousa@gmail.com>
2024-12-13 19:25:47 -0300

    editando algumas linhas de código

--- identify_faces.py
+++ identify_faces.py
@@ -56,6 +56,8 @@ def identificar_rostos():
 
     # Inicializa captura de vídeo
     video_capture = cv2.VideoCapture(0)
+    video_capture.set(3, 1280)
+    video_capture.set(4, 720)
 
     if not video_capture.isOpened():
         print('Erro ao acessar a câmera. Certifique-se de que ela está conectada e funcionando.')
